84c7ef06c967debb36172e68cb3617a5962fe0a8
yshr-926 <hiromari0926@gmail.com>
2023-10-09 15:49:43 +0900

    Add SAM optimizer

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,5 +1,6 @@
 from optimizers.lars import LARS
 from optimizers.lamb import Lamb
+from optimizers.sam import SAM, SAM2
 from optimizers.ada_inverse import Adainverse
 from optimizers.ada_inverse2 import Adainverse2
 from optimizers.adasam import Adasam
@@ -9,6 +10,8 @@ from optimizers.lr_scheduler import WarmupPolynomialLR, LinearWarmupCosineAnneal
 __all__ = [
     "LARS",
     "Lamb",
+    "SAM",
+    "SAM2",
     "Adainverse",
     "Adainverse2",
     "Adasam",
