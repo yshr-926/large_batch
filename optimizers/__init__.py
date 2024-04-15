12d3c8a51d9e0738590d4a64a4c19aa678d94bef
yshr-926 <hirotonpp.6290@gmail.com>
2024-04-16 04:42:31 +0900

    Add SAM_ON

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -2,6 +2,7 @@ from optimizers.lars import LARS
 from optimizers.lamb import Lamb
 from optimizers.sam import SAM, SAM2, SAM_L2
 from optimizers.sam_sgd import SAM_SGD
+from optimizers.sam_on import SAM_ON, ASAM_ON
 from optimizers.ada_inverse import Adainverse
 from optimizers.lr_scheduler import WarmupPolynomialLR, LinearWarmupCosineAnnealingLR, linear_warmup_decay
 
@@ -12,6 +13,8 @@ __all__ = [
     "SAM2",
     "SAM_L2",
     "SAM_SGD",
+    "SAM_ON",
+    "ASAM_ON",
     "Adainverse",
     "WarmupPolynomialLR",
     "LinearWarmupCosineAnnealingLR",
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,14 @@
+import os
 import models
 import torch
 import torch.nn.functional as F
-from torch import nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-import torchvision.models
+from torch.utils.data.sampler import BatchSampler
+import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from utils.batch_cutout import batch_Cutout
 from utils.cutout_sam import Cutout_Sam
-import torchvision.datasets as datasets
 from optimizers import *
 
 class DatasetWithIndex:
@@ -26,70 +26,60 @@ class DatasetWithIndex:
     def classes(self):
         return self.dataset.classes
 
+class MyBatchSampler(BatchSampler):
+  def __init__(self, sampler, batch_size, aug_size):
+    self.sampler = sampler
+    self.batch_size = batch_size
+    self.aug_size = aug_size
+  def __iter__(self):
+    batch = [0] * self.batch_size
+    idx_in_batch = 0
+    for idx in self.sampler:
+      batch[idx_in_batch] = idx
+      idx_in_batch += 1
+      if idx_in_batch == self.batch_size:
+        yield batch*self.aug_size
+        idx_in_batch = 0
+        batch = [0] * self.batch_size
+    if idx_in_batch > 0:
+        yield batch[:idx_in_batch]*self.aug_size
+
 def get_model(model, num_class, dataset):
-    if model == 'ResNet18':
+    if 'ResNet' in model:
+        resnet_number = int(model[6:])
+        assert resnet_number in [18, 34, 50, 101, 152], f"Invalid ResNet model number: {resnet_number}"
         return models.__dict__[model](num_classes=num_class, dataset=dataset)
-    elif model == 'ResNet50':
-        return models.__dict__[model](num_classes=num_class, dataset=dataset)
-    elif model == 'ResNet101':
-        return models.__dict__[model](num_classes=num_class, dataset=dataset)
-    elif model == 'WideResNet28-2':
-        return models.__dict__['WRN28_2'](num_classes=num_class)
     elif model == 'WideResNet28-10':
         return models.__dict__['WRN28_10'](num_classes=num_class)
-    elif model == 'AlexNet':
-        return models.__dict__['AlexNetOrigin'](num_classes=num_class)
-    elif model == 'AlexNetBN':
-        return models.__dict__[model](num_classes=num_class)
-    # elif model == 'PyramidNet':
-    #     return models.__dict__['Pyramid'](num_classes=num_class)
-    # elif model == 'ShakeResNeXt':
-    #     return models.__dict__[model](depth=26, w_base=64, cardinary=4, num_classes=num_class)
-    # elif model == 'ShakePyramidNet':
-        # return torchvision.models.__dict__[model](depth=110, alpha=270, num_classes=num_class)
-    # elif model == 'MobileNetV2':
-        # return torchvision.models.__dict__['mobilenetv2'](num_classes=num_class)
-    # elif model == 'ShakeWideResNet':
-        # return torchvision.models.__dict__[model](depth=28, width_factor=6, dropout=0.0, in_channels=3, num_classes=num_class)
-    # elif model == 'EfficientNet':
-        # return torchvision.models.__dict__[model].from_name('efficientnet-b7', num_classes=num_class)
+    elif model == 'PyramidNet':
+        return models.__dict__['Pyramid'](num_classes=num_class)
+    elif model == 'ShakePyramidNet':
+        return models.__dict__['ShakePyramidNet272_200'](num_classes=num_class)
+    elif model == 'ShakeWideResNet':
+        return models.__dict__[model](depth=28, width_factor=10, dropout=0.0, in_channels=3, num_classes=num_class)
+    elif model == 'ShakeShake26-32':
+        return models.__dict__['ShakeShake26_2x32d'](num_classes=num_class)
+    elif model == 'ShakeShake26-64':
+        return models.__dict__['ShakeShake26_2x64d'](num_classes=num_class)
+    elif model == 'ShakeShake26-96':
+        return models.__dict__['ShakeShake26_2x96d'](num_classes=num_class)
 
 def get_optimizer(args, model):
     if args.optimizer == 'SGD':
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    elif args.optimizer == 'Adam':
+        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    elif args.optimizer == 'Adagrad':
+        optimizer = optim.Adagrad(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     elif args.optimizer == 'LARS':
         optimizer = LARS(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     elif args.optimizer == 'Lamb':
         optimizer = Lamb(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-    elif args.optimizer == 'Adagrad':
-        optimizer = optim.Adagrad(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    elif args.optimizer == 'Adaggrad':
-        optimizer = Adaggrad(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     elif args.optimizer == 'Adainverse':
         optimizer = Adainverse(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    elif args.optimizer == 'Adainverse2':
-        optimizer = Adainverse2(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    elif args.optimizer == 'Adasam':
-        optimizer = Adasam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, gn=args.gn)
     return optimizer
 
-def get_transforms(model, dataset):
-    if model == 'AlexNetBN':
-        transform_train = transforms.Compose([
-            transforms.Resize((256, 256), interpolation=transforms.InterpolationMode.BICUBIC),
-            transforms.RandomCrop(224, padding=28),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            # batch_Cutout(n_holes=1, length=16),
-        ])
-        transform_test = transforms.Compose([
-            transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.BICUBIC),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-        return transform_train, transform_test
-    
+def get_transforms(dataset, train_policy, test_policy):
     if dataset == 'ImageNet':
         transform_train = transforms.Compose([
             transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.BICUBIC),
@@ -120,19 +110,26 @@ def get_transforms(model, dataset):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-    elif dataset == 'CIFAR100' or 'CIFAR10':
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[x / 255.0 for x in[125.3, 123.0, 113.9]], std=[x / 255.0 for x in [63.0, 62.1, 66.7]]),
-            batch_Cutout(n_holes=1, length=16),
-        ])
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[x / 255.0 for x in[125.3, 123.0, 113.9]], std=[x / 255.0 for x in [63.0, 62.1, 66.7]]),
-        ])
-    
+    elif dataset == 'CIFAR100' or dataset == 'CIFAR10':
+        #define dataset and transform
+        for i, policy in enumerate([train_policy, test_policy]):
+            transform_list=[]
+            if policy == 'policy3':
+                transform_list.append(transforms.AutoAugment(policy=transforms.autoaugment.AutoAugmentPolicy.CIFAR10))
+            if policy == 'policy1' or policy == 'policy2' or policy == 'policy3':
+                transform_list.extend([transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip()])
+            if dataset == 'CIFAR100':
+                transform_list.extend([transforms.ToTensor(), transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])])
+            elif dataset == 'CIFAR10':
+                transform_list.extend([transforms.ToTensor(), transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])])
+            if policy == 'policy2' or policy == 'policy3':
+                transform_list.append(batch_Cutout(n_holes=1, length=16))
+            
+            if i == 0:
+                transform_train = transforms.Compose(transform_list)
+            elif i == 1:
+                transform_test = transforms.Compose(transform_list)
+
     return transform_train, transform_test
 
 def get_sam_transform(dataset):
@@ -176,8 +173,17 @@ def smooth_crossentropy(pred, gold, smoothing=0.1):
 
     return F.kl_div(input=log_prob, target=one_hot, reduction='none').sum(-1)
 
+def get_unique_filename(filename):
+    base, ext = os.path.splitext(filename)
+    i = 1
+    while os.path.exists(filename):
+        filename = f"{base}_{i}{ext}"
+        i += 1
+    return filename
+
 def print_header():
-    print(f"┏━━━━━━━━━━━━━━┳━━━━━━━╸S╺╸T╺╸A╺╸T╺╸S╺━━━━━━━┳━━━━━━━╸T╺╸R╺╸A╺╸I╺╸N╺━━━━━━━┳━━━━━━━╸V╺╸A╺╸L╺╸I╺╸D╺━━━━━━━┳━━━╸A╺╸V╺╸E╺╸R╺╸A╺╸G╺╸E╺╸D━━━┓")
-    print(f"┃              ┃              ╷              ┃              ╷              ┃              ╷              ┃              ╷              ┃")
-    print(f"┃       epoch  ┃        lr    │       time   ┃        loss  │    accuracy  ┃        loss  │    accuracy  ┃        loss  │    accuracy  ┃")
-    print(f"┠──────────────╂──────────────┼──────────────╂──────────────┼──────────────╂──────────────┼──────────────┨──────────────┼──────────────┨")
+    header = f"{'epoch':^10} | {'lr':^10} | {'time':^10} | {'loss':^10} | {'acc':^10} | {'test_loss':^10} | {'test_acc':^10} | {'ave_loss':^10} | {'ave_acc':^10}"
+    separator = "-" * len(header)
+    print(separator)
+    print(header)
+    print(separator)
